729d070be133e8b297b3fb1493e91bcd2b4a95fd
ddzy <1766083035@qq.com>
2024-04-12 15:26:07 +0800

    feat: 【文章详情页】无需 token 鉴权

--- uniCloud-aliyun/cloudfunctions/get-article/index.js
+++ uniCloud-aliyun/cloudfunctions/get-article/index.js
@@ -3,12 +3,6 @@
 const cloudUtils = require('common-cloud-utils');
 
 exports.main = async (event, context) => {
-	// token验证
-	const verify = await cloudUtils.jwt.verifyToken(event.token);
-	if (verify.code !== 0) {
-		return verify;
-	}
-
 	const db = await uniCloud.databaseForJQL({
 		event,
 		context,
