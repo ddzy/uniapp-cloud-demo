70e5d1767312186d588a061962e8489361b7f971
ddzy <1766083035@qq.com>
2024-04-25 10:46:26 +0800

    feat(refreshtoken): 客户端监听 onRefreshToken 事件，token 刷新时重新存储 token，并且重新获取客户端唯一推送标识 push_clientid

--- App.vue
+++ App.vue
@@ -1,11 +1,25 @@
-<script>
+<script lang="ts">
+import constants from './constants/index';
+
 export default {
 	onLaunch: function () {
 		console.log('App Launch');
 	},
-	onShow: function () {
-		console.log('App Show');
+	onShow: async function () {
 		this.$store.dispatch('user/DISPATCH_USER_INFO');
+		this.$store.dispatch('chat/DISPATCH_PUSH_CLIENT_ID');
+
+		uniCloud.onRefreshToken((result) => {
+			// refreshToken刷新成功，存储token
+			uni.setStorageSync(constants.token.TOKEN, result?.token ?? '');
+			uni.setStorageSync(
+				constants.token.TOKEN_EXPIRED,
+				result?.tokenExpired || 0,
+			);
+			// 重新获取客户端唯一推送标识
+			this.$store.dispatch('chat/DISPATCH_PUSH_CLIENT_ID');
+			this.$store.commit('user/UPDATE_IS_LOGINED', true);
+		});
 	},
 	onHide: function () {
 		console.log('App Hide');
